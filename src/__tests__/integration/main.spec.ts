8e1a111023b3f47d1479205ba9a7c388e1134f93
Dmytrii Savchyk <dmytrii.savchyk@nextiva.com>
2023-03-02 14:17:25 -0700

    updated prettier

--- src/__tests__/integration/main.spec.ts
+++ src/__tests__/integration/main.spec.ts
@@ -1,12 +1,12 @@
-import execa from 'execa';
-import { resolve } from 'path';
+import execa from 'execa'
+import { resolve } from 'path'
 
-const bin = resolve(__dirname, './bin.js');
+const bin = resolve(__dirname, './bin.js')
 
 describe('my-command', () => {
   it('should display the help contents', async () => {
-    const { stdout } = await execa(bin, ['--help']);
+    const { stdout } = await execa(bin, ['--help'])
 
-    expect(stdout).toContain('Usage: my-command [options]');
-  });
-});
+    expect(stdout).toContain('Usage: my-command [options]')
+  })
+})
